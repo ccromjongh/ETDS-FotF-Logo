53be2da2a9857a20c309f34eeeeaa0063aad41b9
Casper Cromjongh <casper.cromjongh@hotmail.com>
2022-09-28 17:14:25 +0200

    Add Serial commands.
    These should make it easier to find LED positions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Streaming.h>
 // Not actually using SPI, just here to shut the message up
 #define SPI_DATA 23
 #define SPI_CLOCK 18
@@ -40,24 +41,108 @@ struct StripMapping mappings[] = {
 
 };
 
-CRGB LED1[200];
-CRGB LED2[200];
-CRGB LED3[200];
-CRGB LED4[200];
-CRGB* real_leds[] = {
-        reinterpret_cast<CRGB *>(&LED1),
-        reinterpret_cast<CRGB *>(&LED2),
-        reinterpret_cast<CRGB *>(&LED3),
-        reinterpret_cast<CRGB *>(&LED4),
-};
+void parse_serial();
+void map_leds(struct StripMapping mapping);
+
+// Definition of physical strand lengths
+const int NUM_STRANDS = 4;
+const int physical_sizes[NUM_STRANDS] = {200, 200, 200, 200};
+CRGB* real_leds[NUM_STRANDS] = {};
 
 void setup() {
+    Serial.begin(115200);
+    Serial.println("Booting");
+
+    // Initialize LED arrays
+    for (int i = 0; i < NUM_STRANDS; ++i) {
+        real_leds[i] = static_cast<CRGB *>(malloc(physical_sizes[i] * sizeof(CRGB)));
+    }
+
     FastLED.addLeds<LED_TYPE,DATA_PIN_1,COLOR_ORDER>(real_leds[0], NUM_LEDS).setCorrection(TypicalLEDStrip);
     FastLED.addLeds<LED_TYPE,DATA_PIN_2,COLOR_ORDER>(real_leds[1], NUM_LEDS).setCorrection(TypicalLEDStrip);
     FastLED.addLeds<LED_TYPE,DATA_PIN_3,COLOR_ORDER>(real_leds[2], NUM_LEDS).setCorrection(TypicalLEDStrip);
     FastLED.addLeds<LED_TYPE,DATA_PIN_4,COLOR_ORDER>(real_leds[3], NUM_LEDS).setCorrection(TypicalLEDStrip);
 
     /// Initialize network stuff here
+}
+
+// Variables related to parsing serial input
+char input_buffer[30];
+const char top[] = "\x1B\x5B\x41";
+const char down[] = "\x1B\x5B\x42";
+const char right[] = "\x1B\x5B\x43";
+const char left[] = "\x1B\x5B\x44";
+const char page_up[] = "\x1B\x5B\x35\x7E";
+const char page_down[] = "\x1B\x5B\x36\x7E";
+
+// Used for finding the LED mapping indexes
+int mode = 0;
+int selected = 0;
+int positions[NUM_STRANDS] = {0};
+
+void loop() {
+    parse_serial();
+
+    /** Receive/create the colors for the virtual strip here **/
+
+    /** Map the virtual strips to the real strips **/
+    for (auto mapping: mappings) {
+        // Write mapping here
+        map_leds(mapping);
+    }
+
+    FastLED.show();
+    FastLED.delay(1000/FRAMES_PER_SECOND);
+}
+
+/** Modulo that only returns positive results **/
+inline int pos_mod(int i, int n) {
+    return (i % n + n) % n;
+}
+
+/**
+ * Detects serial commands and acts accordingly
+ */
+void parse_serial() {
+    if (Serial.available()){
+        Serial.read(input_buffer, sizeof input_buffer);
+
+        if (strcmp(input_buffer, top) == 0) {
+            // Serial << "Up detected!" << endl;
+            positions[selected] = pos_mod(positions[selected]+1, physical_sizes[selected]);
+        } else if (strcmp(input_buffer, down) == 0) {
+            // Serial << "Down detected!" << endl;
+            positions[selected] = pos_mod(positions[selected]-1, physical_sizes[selected]);
+        } else if (strcmp(input_buffer, page_up) == 0) {
+            // Serial << "Page Up detected!" << endl;
+            positions[selected] = pos_mod(positions[selected]+10, physical_sizes[selected]);
+        } else if (strcmp(input_buffer, page_down) == 0) {
+            // Serial << "Page Down detected!" << endl;
+            positions[selected] = pos_mod(positions[selected]-10, physical_sizes[selected]);
+        } else if (strcmp(input_buffer, right) == 0) {
+            // Serial << "Right detected!" << endl;
+            selected = pos_mod(selected+1, NUM_STRANDS);
+        } else if (strcmp(input_buffer, left) == 0) {
+            // Serial << "Left detected!" << endl;
+            selected = pos_mod(selected-1, NUM_STRANDS);
+        } else {
+            Serial << "Input detected: ";
+            for (int i = 0; i < strlen(input_buffer); ++i) {
+                Serial << _HEX(input_buffer[i]) << " '" << input_buffer[i] << "' ";
+            }
+            for (int i = 0; i < strlen(input_buffer); ++i) {
+                Serial.print(input_buffer[i], HEX);
+            }
+            Serial << endl;
+        }
+        memset(input_buffer, 0, sizeof input_buffer);
+
+        Serial << "Selected: " << selected << ", positions: ";
+        for (auto pos : positions) {
+            Serial << pos << " ";
+        }
+        Serial << endl;
+    }
 }
 
 /**
@@ -83,17 +168,4 @@ void map_leds(struct StripMapping mapping) {
         // Do the interpolation
         virtual_leds[mapping.strip][i] = blend(c1, c2, remainder);
     }
-}
-
-void loop() {
-    /** Receive/create the colors for the virtual strip here **/
-
-    /** Map the virtual strips to the real strips **/
-    for (auto mapping: mappings) {
-        // Write mapping here
-        map_leds(mapping);
-    }
-
-    FastLED.show();
-    FastLED.delay(1000/FRAMES_PER_SECOND);
 }
